64df1db4b9fb52a17f2ab047b1da17c82b818647
Alberto <amedinag43@gmail.com>
2025-03-22 09:56:30 -0600

    Agregar archivos develop funciones

--- funciones.py
+++ funciones.py
@@ -1,7 +1,13 @@
 def my_funct():
     print("Hola mundo")
     
+
 def my_function_strng(x,y):
     print("Hola mundo")
     result=x+y
     return result
+
+def my_funct_2()
+    print("Hola 2")
+    
+
